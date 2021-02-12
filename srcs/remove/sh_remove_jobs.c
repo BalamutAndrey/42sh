f940a2edc57cd348a31563fe605ea5d8616f125d
Earlean Boris <eboris@il-k1.msk.21-school.ru>
2021-02-12 17:11:38 +0300

    Norme...

--- srcs/remove/sh_remove_jobs.c
+++ srcs/remove/sh_remove_jobs.c
@@ -3,26 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   sh_remove_jobs.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: geliz <geliz@student.42.fr>                +#+  +:+       +#+        */
+/*   By: eboris <eboris@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 00:38:27 by geliz             #+#    #+#             */
-/*   Updated: 2021/02/09 00:41:22 by geliz            ###   ########.fr       */
+/*   Updated: 2021/02/12 17:11:13 by eboris           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh_main.h"
 
-void    sh_remove_jobs(t_main *main)
+void	sh_remove_jobs(t_main *main)
 {
-    t_jobs  *tmp;
-    t_jobs  *nxt;
+	t_jobs	*tmp;
+	t_jobs	*nxt;
 
-    tmp = main->jobs;
-    while (tmp)
-    {
-        nxt = tmp->next;
-        sh_job_remove(tmp);
-        tmp = nxt;
-    }
-    main->jobs = NULL;
+	tmp = main->jobs;
+	while (tmp)
+	{
+		nxt = tmp->next;
+		sh_job_remove(tmp);
+		tmp = nxt;
+	}
+	main->jobs = NULL;
 }
